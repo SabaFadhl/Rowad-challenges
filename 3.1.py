9ea4de88a5dcd96af9724ac347b7b9bf07758d38
Saba Fadhl <sabafadl98@gmail.com>
2022-01-10 14:17:55 +0300

    Question 1 python lec3 'if'

---
+++ 3.1.py
@@ -0,0 +1,4 @@
+colors=["blue", "BLUE","Blue"]
+color=input("enter your favourite colour.")
+if color in colors:print("I like blue too")
+else:print("I don’t like {0}, I prefer blue.".format(color))
