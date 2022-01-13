5742fc72c9af286872206d62bdb4f65c833adc84
Saba Fadhl <sabafadl98@gmail.com>
2022-01-13 13:31:11 +0300

    Question 4 python lec4 'functions args *'

--- 6.1.py
+++ 6.1.py
@@ -1,13 +1,20 @@
-#Function to add 0 or more numbers way 1
+#Function to add 0 or more numbers way 1 --- minimum 0 args ----
 def add(*s):
     return sum(s)
 
 print(add(2,3))
 print(add(2,3,4))
 
-#Function to add 0 or more numbers way 2
+#Function to add 0 or more numbers way 2 --- minimum 1 args -----
 def add(x,*s):
     return x+sum(s)
+
+print(add(2,3))
+print(add(2,3,4))
+
+#Function to add 0 or more numbers way 3 --- minimum 2 args ----
+def add(x,y,*s):
+    return x+y+sum(s)
 
 print(add(2,3))
 print(add(2,3,4))
