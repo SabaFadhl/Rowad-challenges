52fc0f64c311e325cc3debc6ff7dfad43b36c0c3
Saba Fadhl <sabafadl98@gmail.com>
2022-01-16 20:05:52 +0300

    Question 1 and 2 lec 7

--- 7.1&7.2.py
+++ 7.1&7.2.py
@@ -2,16 +2,19 @@
 # str_list = ["I", "Love", "Yemen"]
 # print(" ".join(str_list))
 #Question 2
-while True:
-    try:
-        num=int(input("please enter number or 0 to exit : "))
-        
-        if num==0:
-            break
-        elif len(str(num))<3 :
-            print(" enter number consist from 3 or more digits ") 
-        else:
-            print(str(num)[2])
-            
-    except:
-        print("enter number !!!")
+
+#functiom
+def third():
+    while True:
+        try:
+            num=int(input("please enter number or 0 to exit : "))
+            if num==0:
+                break
+            elif len(str(num))<3 :
+                print(" enter number consist from 3 or more digits ") 
+            else:
+                print(str(num)[2])
+        except:
+            print("enter number !!!")
+ #calling           
+third()
