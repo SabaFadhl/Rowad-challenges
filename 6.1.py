047ae3de3c91b7f349d9bd3ceb5ae0226d827500
Saba Fadhl <sabafadl98@gmail.com>
2022-01-16 15:45:21 +0300

    Question 1 and 2

--- 6.1.py
+++ 6.1.py
@@ -1,20 +1,22 @@
-#Function to add 0 or more numbers way 1 --- minimum 0 args ----
-def add(*s):
-    return sum(s)
+# #Function to add 0 or more numbers way 1 --- minimum 0 args ----
+# def add(*s):
+#     return sum(s)
 
-print(add(2,3))
-print(add(2,3,4))
+# print(add(2,3))
+# print(add(2,3,4))
 
-#Function to add 0 or more numbers way 2 --- minimum 1 args -----
-def add(x,*s):
-    return x+sum(s)
+# #Function to add 0 or more numbers way 2 --- minimum 1 args -----
+# def add(x,*s):
+#     return x+sum(s)
 
-print(add(2,3))
-print(add(2,3,4))
+# print(add(2,3))
+# print(add(2,3,4))
+# #
+# #Function to add 0 or more numbers way 3 --- minimum 2 args ----
+# def add(x,y,*s):
+#     return x+y+sum(s)
 
-#Function to add 0 or more numbers way 3 --- minimum 2 args ----
-def add(x,y,*s):
-    return x+y+sum(s)
-
-print(add(2,3))
-print(add(2,3,4))
+# print(add(2,3))
+# print(add(2,3,4))
+import math
+print(math)
