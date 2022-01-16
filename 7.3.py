17d83a640dff6b96a40972c3810fdbc1344868e0
Saba Fadhl <sabafadl98@gmail.com>
2022-01-16 15:56:10 +0300

    Crossword puzzles program

---
+++ 7.3.py
@@ -0,0 +1,18 @@
+Words_list = [
+     {"word": "CPU" , "question": "Brain of computer is?"},
+     {"word": "Camel" , "question": "Which animal has hump on its back?"},
+     {"word": "Jasmine" , "question": "flower is white in colour?"},
+     {"word": "Oval" , "question": "Shape of Egg is?"},
+     {"word": "Winter" , "question": " In which season we wear warm clothes?"},
+     {"word": "Seven" , "question": "colors are there in a rainbow?"},
+     {"word": "Red" , "question": "primary color?"},
+     {"word": "Sun" , "question": "principal source of energy for earth?"},
+     {"word": "Africa" , "question": "continent is known as ‘Dark’ continent?"},
+     {"word": "Asia" , "question": "the largest continent in the world?"},
+     {"word": "Yemen" , "question": "Arabic Country?"},
+     {"word": "easy"  , "question": "simple think?"},
+     {"word": "Frog"  , "question": " Animal live in the sea?"}
+     {"word": "Ring"  , "question": "something worn by hand?"},,
+     {"word": "Year"  , "question": "12 months ?"}
+     
+  ]
